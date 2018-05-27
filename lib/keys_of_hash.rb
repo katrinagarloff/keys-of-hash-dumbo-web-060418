865116b1874900610946c3c9bad87158f46f415f
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-27 00:49:48 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,13 @@
 class Hash
   def keys_of(*values)
-    array_of_values
+    array_of_values = []
     values.each do |value|
-      puts value
+    self.each do |k, v|
+      if value == v 
+        array_of_values << k
+      end
     end
   end
-  keys_of("meow", 3, "pih-chow!")
+  #keys_of("meow", 3, "pih-chow!")
 end
 
